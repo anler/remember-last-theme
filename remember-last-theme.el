57e8e2a475ea89316dbb5c4d2ea047f56a2cbcdf
Anler Hernández Peral <inbox@anler.me>
2017-06-19 23:33:09 +0200

    Add option to save themes in external file

--- remember-last-theme.el
+++ remember-last-theme.el
@@ -1,9 +1,9 @@
-;;; remember-last-theme.el --- Remember the last used theme between sessions.
+;;; remember-last-theme.el --- Remember the last used theme between sessions. -*- lexical-binding: t-*-
 ;;
 ;; Author: Anler Hernández Peral <inbox+emacs@anler.me>
 ;; Maintainer: Anler Hernández Peral <inbox+emacs@anler.me>
 ;; Created: 26 Feb 2017
-;; Version: 1.0.0
+;; Version: 1.1.0
 ;; Keywords: convenience faces
 ;; URL: https://github.com/anler/remember-last-theme
 ;; Package-Requires: ((emacs "24.4"))
@@ -41,11 +41,42 @@
   (interactive)
   (customize-save-variable 'custom-enabled-themes custom-enabled-themes))
 
+(defun remember-last-theme-with-file-save (filename)
+  "Save the current theme(s) for next sessions."
+  (defun perform-save ()
+    (interactive)
+    (with-temp-buffer
+      (print custom-enabled-themes (current-buffer))
+      (if (file-writable-p filename)
+          (write-file filename)
+        (message (format "Cannot save themes because %s is not writable" filename))))))
+
+(defun remember-last-theme-with-file-load (filename)
+  "Load the current theme(s) for current session."
+  (defun perform-load ()
+    (interactive)
+    (if (file-readable-p filename)
+        (mapc
+         'load-theme
+         (read (with-temp-buffer
+                 (insert-file-contents filename)
+                 (buffer-string))))
+      (message (format "Cannot load saved themes because %s is not readable" filename)))))
+
 ;;;###autoload
 (defun remember-last-theme-enable ()
   "Ensure that the current theme(s) will be saved when Emacs exits."
   (interactive)
   (add-hook 'kill-emacs-hook 'remember-last-theme-save))
+
+;;;###autoload
+(defun remember-last-theme-with-file-enable (filename)
+  "Ensure that the current theme(s) will be saved when Emacs exits."
+  (interactive "F")
+  (message filename)
+  (add-hook 'kill-emacs-hook (remember-last-theme-with-file-save filename))
+  (add-hook 'after-init-hook (remember-last-theme-with-file-load filename))
+  )
 
 (provide 'remember-last-theme)
 ;;; remember-last-theme.el ends here
